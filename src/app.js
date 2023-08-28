635dbbaf7051b94a84dde2bde72c95a2119ec8f1
Juan Alzate <139303318+juanchoalzate@users.noreply.github.com>
2023-08-28 22:26:03 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generadorexcusas();
+  });
+
+  console.log("Hello Juancho from the console! ");
+};
+
+let generadorexcusas = () => {
+  let pronoun = ["Un", "El"];
+  let subject = ["cartero", "gato", "perro", "conductor", "portero"];
+  let action = ["tomo mi", "lanzo mi", "le grito a mi", "me robo", "mordio mi"];
+  let possetion = ["tarea", "zapato", "auto", "pie"];
+  let where = ["en la calle", "en mi casa", "en el camino a casa"];
+
+  let proIndx = Math.floor(Math.random() * pronoun.length);
+  let subjtIndx = Math.floor(Math.random() * subject.length);
+  let actionindx = Math.floor(Math.random() * action.length);
+  let possetionindex = Math.floor(Math.random() * possetion.length);
+  let whereindex = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[proIndx] +
+    " " +
+    subject[subjtIndx] +
+    " " +
+    action[actionindx] +
+    " " +
+    possetion[possetionindex] +
+    " " +
+    where[whereindex]
+  );
 };
